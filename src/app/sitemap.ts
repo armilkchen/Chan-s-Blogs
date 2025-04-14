4593edbc7b578fa79926ae29c0cd2f3d80707012
chenzijie <chenzijie@sics.ac.cn>
2025-04-14 17:14:16 +0800

    fix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,25 +4,19 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
   {
     "url": "/",
-    lastModified: new Date("2025-04-05T13:14:53.941Z"),
+    lastModified: new Date("2025-04-14T09:13:35.824Z"),
     "changeFrequency": "daily",
     "priority": 1
   },
   {
     "url": "/blog",
-    lastModified: new Date("2025-04-05T13:14:53.942Z"),
+    lastModified: new Date("2025-04-14T09:13:35.825Z"),
     "changeFrequency": "daily",
     "priority": 0.9
   },
   {
-    "url": "blog/hello-world",
-    lastModified: new Date("2025-04-05T12:10:00.000Z"),
-    "changeFrequency": "weekly",
-    "priority": 0.8
-  },
-  {
-    "url": "blog/intro",
-    lastModified: new Date("2025-04-05T12:10:00.000Z"),
+    "url": "blog/20250414-1",
+    lastModified: new Date("2025-04-14T12:10:00.000Z"),
     "changeFrequency": "weekly",
     "priority": 0.8
   }
